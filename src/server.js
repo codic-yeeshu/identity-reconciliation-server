825bb82e3e0d81c048769608ce7473251ab02ba6
codic-yeeshu <yeeshu.exploresmore@gmail.com>
2024-11-24 17:35:26 +0530

    added  keep alive endpoint to keep the server alive

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import identifyUser from "./routes/identifyUser.js";
 import user from "./routes/user.js";
 import { connectDB } from "./config/db.js";
+import axios from "axios";
 
 configDotenv();
 const app = express();
@@ -17,6 +18,27 @@ app.get("/", (req, res) => {
 
 app.use("/identify", identifyUser);
 app.use("/getAllUser", user);
+
+// Keep-alive endpoint
+app.get("/keep-alive", (req, res) => {
+  console.log("alive");
+  res.send("Server is alive!");
+});
+
+// Function to call the keep-alive endpoint
+const keepServerAlive = async () => {
+  try {
+    const url = `identity-reconciliation-server-api.onrender.com/keep-alive`;
+    await axios.get(url);
+    console.log(`Keep-alive ping sent to ${url}`);
+  } catch (error) {
+    console.error("Error sending keep-alive ping:", error.message);
+  }
+};
+
+// Start the keep-alive pings every minute
+setInterval(keepServerAlive, 1 * 60 * 1000);
+keepServerAlive();
 
 connectDB().then(() => {
   app.listen(PORT, () => {
